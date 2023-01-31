227107759cde8c7a16aaa16fcab3e2b2d375c549
Endobyte <endobyte@gmail.com>
2023-01-31 11:37:23 -0800

    Make some progress on reviews

---
+++ controllers/reviewController.js
@@ -0,0 +1,16 @@
+const {Product, Review} = require('../models')
+
+module.exports.renderAddReviewForm = async function (req, res) {
+    const product = await Product.findByPk(
+        req.params.product_id
+    );
+    const review = {
+        customer_name: '',
+        subject: '',
+        rating: 1,
+        description: ''
+    }
+    res.render('reviews/add', {product, review});
+}
+
+//CONTINUE ON SLIDE 55
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const productController = require('../controllers/productController')
+const reviewController = require('../controllers/reviewController')
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -10,7 +11,8 @@ router.get('/products', productController.viewProducts)
 router.get('/products/profile/:id', productController.renderProfile);
 router.get('/products/edit/:id', productController.renderEditForm);
 router.get('/products/add', productController.renderAddForm);
-router.get('/products/delete/:id', productController.deleteProduct)
+router.get('/products/delete/:id', productController.deleteProduct);
+router.get('/products/:productId/review/add', reviewController.renderAddViewForm);
 router.post('/products/edit/:id', productController.updateProduct);
 router.post('/products/add', productController.addProduct);
 
