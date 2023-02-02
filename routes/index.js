dea5490b0ff477b6fee6e4bee28db31a7bed97a0
Endobyte <endobyte@gmail.com>
2023-02-02 11:30:39 -0800

    Finish functionality of site

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,12 @@ router.get('/products/profile/:id', productController.renderProfile);
 router.get('/products/edit/:id', productController.renderEditForm);
 router.get('/products/add', productController.renderAddForm);
 router.get('/products/delete/:id', productController.deleteProduct);
-router.get('/products/:productId/review/add', reviewController.renderAddViewForm);
+router.get('/products/:productId/review/add', reviewController.renderAddReviewForm);
+router.get('/reviews/edit/:id', reviewController.renderEditReviewForm);
+router.get('/reviews/delete/:id', reviewController.deleteReview);
+router.post('/products/:productId/review/add', reviewController.addReview);
 router.post('/products/edit/:id', productController.updateProduct);
 router.post('/products/add', productController.addProduct);
+router.post('/reviews/edit/:id', reviewController.updateReview);
 
 module.exports = router;
